9cfe4dccf137e240288b33715e8260eb1a6ea6d1
Nikita Ostapenko <nikitul2005@gmail.com>
2025-09-19 13:27:23 -0400

    add a button to app to switch between multi and single camera view

--- client/vite/src/App.jsx
+++ client/vite/src/App.jsx
@@ -1,16 +1,27 @@
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { MediaPlayer } from "dashjs"
 import './App.css'
 import SingleVideo from './components/SingleVideo'
-
+import AllVideos from './components/AllVideos' 
 function App() {
   const player = useMemo(() => {
     return MediaPlayer().create();
   }, []);
+  const [singleVideo, setSingleVideo] = useState(true);
   return (
-    <>
-      <SingleVideo player={player} />
-    </>
+	<>
+		{singleVideo ? 
+			<>
+				<button onClick={() => setSingleVideo(false)}>Switch to Multi Video</button>
+				<SingleVideo player={player} />
+			</>
+       		:
+			<>
+				<button onClick={() => setSingleVideo(true)}>Switch to Single Video</button>
+				<AllVideos player={player} />
+			</>
+		}
+	</>
   )
 }
 
