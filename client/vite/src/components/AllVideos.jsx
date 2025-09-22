a262ee0e38d55fd5b886b7621080f749e1f72739
Nikita Ostapenko <nikitul2005@gmail.com>
2025-09-21 22:46:51 -0400

    make Allvideos component

--- client/vite/src/components/AllVideos.jsx
+++ client/vite/src/components/AllVideos.jsx
@@ -1,5 +1,19 @@
-function AllVideos() {
-
-	return <p>hi</p>
+import DashVideo from './DashVideo';
+import React, { useState, useEffect } from 'react';
+function AllVideos({player, cameras, setCameras, getCameras}) {
+    return (
+	<>
+	  {cameras.length > 0 &&
+	    <div style={{display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))', gap: '10px'}}>
+	      {cameras.map((camera, index) => (
+		<div key={index} style={{border: '1px solid black', padding: '10px'}}>
+		  <h3>Camera {camera}</h3>
+		  <DashVideo player={player} url={`/dash/${camera}/manifest.mpd`} />
+		</div>
+	      ))}
+	    </div>
+	  }
+	</>
+    )
 }
 export default AllVideos
